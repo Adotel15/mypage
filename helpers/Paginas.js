e8505f21b9dd48ba23a1eff916ed20600f7298ec
Adotel <adriandotel@gmail.com>
2022-09-04 20:47:03 +0200

    Actualizado Pages

--- helpers/Paginas.js
+++ helpers/Paginas.js
@@ -4,49 +4,56 @@ export const PAGES = [
         nombre: "GuitarLa",
         url: "https://guitarla-three.vercel.app/",
         codigo: "https://github.com/Adotel15/GuitarLa_APP",
-        id: "1"
+        id: "1",
+        texto: "Tienda virual de guitarras, situada en Los Angeles. Compra tu guitarra favorita al mejor precio!",
+        tech: "Next.js, MongoDB, Strapi",
+        foto: "guitar.png"
         
     },
     {
         nombre: "Veterinario",
         url: " https://helpful-griffin-e27f66.netlify.app/",
         codigo: "https://github.com/Adotel15/Veterinario.js",
-        id: "2"
+        id: "2",
+        texto: "Gestiona tu Veterinario de manera eficiente con estas tarjetas interactuables!",
+        tech: "React, TailwindCSS",
+        foto: "veterinario.png"
     },
     {
-        nombre: "Presupuestos",
+        nombre: "MoneyControl",
         url: "https://silver-praline-f96b7a.netlify.app/",
-        codigo: "",
-        id: "3"
+        codigo: "https://github.com/Adotel15/Presupuesto",
+        id: "3",
+        texto: "Gestiona presupuestos para tus proyectos y metas personales.",
+        tech: "React, SASS",
+        foto: "presupuesto.png"
     },
     {
-        nombre: "Criptos",
+        nombre: "CriptoPrecio",
         url: "https://chipper-maamoul-48a347.netlify.app",
         codigo: "https://github.com/Adotel15/Cotizador_Criptos",
-        id: "4"
+        id: "4",
+        texto: "¿Se ha desplomado un 90% el precio de tu cripto favorita? Averígualo en esta app!",
+        tech: "React, Styled Components",
+        foto: "cripto.png"
     }, 
+
     {
-        nombre: "Autos",
-        url: "https://quiet-tulumba-66cfb7.netlify.app/",
-        codigo: "https://github.com/Adotel15/CotizadorAutos",
-        id: "5"
-    },
-    {
-        nombre: "Canciones",
-        url: "https://beamish-palmier-735f25.netlify.app",
-        codigo: "https://github.com/Adotel15/LetrasCanciones",
-        id: "6"
-    },
-    {
-        nombre: "Clima",
-        url: "https://classy-fudge-c179cf.netlify.app/",
+        nombre: "¿Que tiempo hace?",
+        url: "https://master--classy-fudge-c179cf.netlify.app/",
         codigo: "https://github.com/Adotel15/ClimaApp",
-        id: "7"
+        id: "5",
+        texto: "¿Hace frío ?, ¿Hace calor?, ¿Estará lloviendo?, ¿Que tiempo hace en Mongolia? Entra y descúbrelo!",
+        tech: "React, SASS",
+        foto: "clima.png"
     },
     {
-        nombre: "Quisco",
+        nombre: "Fresh Coffe",
         url: "https://quiscoapp-production-e214.up.railway.app/",
         codigo: "https://github.com/Adotel15/QuiscoApp/tree/main",
-        id: "8"
+        id: "6",
+        texto: "Si tienes hambre, entra en Fresh Coffe, así de simple.",
+        tech: "Next.js, MySql, Prisma",
+        foto: "quisco.png"
     }
 ]
